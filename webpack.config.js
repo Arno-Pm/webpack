2bca8fee313f791dbe384fa005e738bf030fbdce
Arno-Pm <70451630+Arno-Pm@users.noreply.github.com>
2022-06-21 16:23:29 +0300

    add files

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+module.exports= {
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/build',
+        filename: '[name]-[fullhash].js',
+        clean: true,
+        publicPath: '/'
+    }, module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: ['@babel/preset-env'],
+                plugins: ['@babel/plugin-transform-runtime'],
+              }
+            }
+          },
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // Creates `style` nodes from JS strings
+              //"style-loader",
+              MiniCssExtractPlugin.loader,
+              // Translates CSS into CommonJS
+              "css-loader",
+              // Compiles Sass to CSS
+              "sass-loader",
+              {
+                loader: "postcss-loader",
+                options: {
+                  postcssOptions: {
+                    plugins: [
+                      [
+                        "postcss-preset-env",
+                        {
+                          // Options
+                        },
+                        ],
+                      ],
+                    },
+                  },
+                },
+              ],
+            },
+      
+          ]
+      },
+      plugins: [
+        new HtmlWebpackPlugin( {
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+          filename: '[name]-[fullhash].css'
+        })
+      ],
+      devServer : {
+        static:{
+          directory: path.join(__dirname, 'src'),
+        },
+        compress:true,
+        port:3000,
+
+      }
+};
